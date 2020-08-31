d6d5919afc0b28c003316521cbec59ff9da767de
Mati <mering20@gmail.com>
2020-08-31 13:44:02 +0200

    add beer bootstrap loader

---
+++ src/main/java/guru/springframework/msscbeerservice/bootstreap/BeerLoader.java
@@ -0,0 +1,46 @@
+package guru.springframework.msscbeerservice.bootstreap;
+
+import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.repositories.BeerRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+@AllArgsConstructor
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+    
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if(beerRepository.count() == 0) {
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000002L)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+        }
+
+        System.out.println("Loaded Beers: " + beerRepository.count());
+    }
+}
