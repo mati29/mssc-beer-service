8b503bc9d80edc67bbec252ad194a2dc4bd06417
Mati <mering20@gmail.com>
2020-09-08 13:25:56 +0200

    add listBeer for http get, and communication with beer inventory

--- src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/msscbeerservice/repositories/BeerRepository.java
@@ -1,9 +1,18 @@
 package guru.springframework.msscbeerservice.repositories;
 
 import guru.springframework.msscbeerservice.domain.Beer;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.UUID;
 
 public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+
+    Page<Beer> findByBeerStyle(String beerStyle, Pageable pageRequest);
+
+    Page<Beer> findByBeerName(String beerName, Pageable pageRequest);
+
+    Page<Beer> findByBeerNameAndBeerStyle(String beerName, String beerStyle, Pageable pageRequest);
 }
