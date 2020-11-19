375a75c7496f016dffc9f3160f7381d18a693fe5
Mati <mering20@gmail.com>
2020-11-19 13:53:50 +0100

    move mysql-init.sql file, add connection to mysql db (dialect MySql8Dialect not working obviously!) repair Beer class to work with mySql

--- src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
+++ src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
@@ -22,7 +22,7 @@ public class Beer {
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     @Type(type="uuid-char")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
 
     @Version
