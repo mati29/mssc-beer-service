ed5e89a0c6ac6ae0a7e22d2c874499308de75441
Mati <mering20@gmail.com>
2020-11-20 11:59:19 +0100

    add beer events

---
+++ src/main/java/guru/springframework/msscbeerservice/events/BeerEvent.java
@@ -0,0 +1,21 @@
+package guru.springframework.msscbeerservice.events;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@RequiredArgsConstructor
+@Builder
+public class BeerEvent implements Serializable {
+
+    private static final long serialVersionUID = 3738178240496566665L;
+
+    private final BeerDto beerDto;
+
+
+
+}
