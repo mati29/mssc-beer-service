5ecc54d59ba650c971caaf46397b5767587c86fb
Mati <mering20@gmail.com>
2020-11-24 16:33:55 +0100

    add validation operation accros 2 microservices

---
+++ src/main/java/guru/springframework/msscbeerservice/services/order/BeerOrderValidation.java
@@ -0,0 +1,30 @@
+package guru.springframework.msscbeerservice.services.order;
+
+import guru.sfg.brewery.model.BeerOrderDto;
+import guru.springframework.msscbeerservice.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class BeerOrderValidation {
+
+    private final BeerRepository beerRepository;
+
+    public Boolean validateOrder(BeerOrderDto beerOrder) {
+        AtomicInteger beersNotFound = new AtomicInteger();
+
+        beerOrder.getBeerOrderLines().forEach(orderLine -> {
+            if(beerRepository.findByUpc(orderLine.getUpc()) == null) {
+                beersNotFound.incrementAndGet();
+            }
+        });
+
+        return beersNotFound.get() == 0;
+    }
+
+}
